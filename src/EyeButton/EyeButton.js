9cd4dff90d595506fe489c92c659c9ba933199e1
Aylan Mello <aylanismello@gmail.com>
2018-10-04 14:33:10 -0700

    switched toggle order;

--- src/EyeButton/EyeButton.js
+++ src/EyeButton/EyeButton.js
@@ -5,7 +5,7 @@ import './EyeButton.css';
 const EyeButton = ({ visualize, toggleVisualize }) => (
 	<div className="EyeButton">
 		<Icon
-			name={visualize ? 'toggle off' : 'toggle on'}
+			name={visualize ? 'toggle on' : 'toggle off'}
 			color="pink"
 			onClick={() => {
 				toggleVisualize();
