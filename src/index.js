aaaaf8b452a2d0285a971dbd55c89d485fc2c5d8
Aylan Mello <aylanismello@gmail.com>
2018-10-22 19:29:52 -0600

    started design pattern for overriding components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import 'semantic-ui-css/semantic.min.css';
 import './index.scss';
 import App from './App/';
+
+WebFont.load({
+  google: {
+    families: ['Open Sans']
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
