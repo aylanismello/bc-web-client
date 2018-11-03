f1a40cd401e55401ac4039a4c43f92b76b851680
Aylan Mello <aylanismello@gmail.com>
2018-11-02 20:57:44 -0600

    aiight

--- src/SplashBanner/SplashBanner.js
+++ src/SplashBanner/SplashBanner.js
@@ -1,27 +1,79 @@
 import React from 'react';
+import logo from './arrow.svg';
 import './splash_banner.scss';
 
-const SplashBanner = () => {
-  return (
-    <div className="SplashBanner">
-      <div className="SplashBanner-image-container">
-        <img src="https://source.unsplash.com/FhdC7RGb5Yg/" className="SplashBanner-image" />
+const Form = ({ handleSubmit, email, updateEmail }) => (
+  <div className="SplashBanner-form-container">
+    <form className="SplashBanner-form" id="1" onSubmit={() => handleSubmit()}>
+      <div className="SplashBanner-email-container">
+        <input
+          type="email"
+          value={email}
+          placeholder="Insert your email..."
+          className="SplashBanner-email"
+          onChange={updateEmail}
+        />
+        <img
+          src={logo}
+          className="SplashBanner-arrow"
+          onClick={() => {
+            // why doesnt this work
+            const el = document.getElementById('1');
+            el.submit();
+          }}
+        />
       </div>
+    </form>
+  </div>
+);
 
-      <div className="SplashBanner-content">
-        <div className="SplashBanner-message-container">
-          <div className="SplashBanner-message-header">Music happens fast</div>
-          <div className="SplashBanner-message-subheader"> GET THE LATEST TRACKS WEEKLY</div>
+const FormSubmitMessage = () => (
+  <div className="SplashBanner-submit-message">
+    <span> Thanks for subscribing! </span>
+    <br />
+    <span> Watch your inbox for weekly 🔥! </span>
+  </div>
+);
+
+class SplashBanner extends React.Component {
+  state = {
+    email: '',
+    submitted: false
+  };
+
+  handleSubmit() {
+    this.setState({ submitted: true });
+  }
+
+  updateEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="SplashBanner">
+        <div className="SplashBanner-image-container">
+          <img src="https://source.unsplash.com/FhdC7RGb5Yg/" className="SplashBanner-image" />
         </div>
 
-        {/*      <div className="SplashBanner-form-container">
-          <form className="SplashBanner-form">
-            <input type="email" className="SplashBanner-email" />
-          </form>
-        </div> */}
+        <div className="SplashBanner-content">
+          <div className="SplashBanner-message-container">
+            <div className="SplashBanner-message-header">Music happens fast</div>
+            <div className="SplashBanner-message-subheader"> GET THE LATEST TRACKS WEEKLY</div>
+          </div>
+          {this.state.submitted ? (
+            <FormSubmitMessage />
+          ) : (
+            <Form
+              email={this.state.email}
+              handleSubmit={() => this.handleSubmit()}
+              updateEmail={e => this.updateEmail(e)}
+            />
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SplashBanner;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WebFont from 'webfontloader';
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
 import './index.scss';
 import App from './App/';
 
