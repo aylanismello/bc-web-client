b5a6d7ec76ae877fbc5bff99a70eafae7d4591dd
Aylan Mello <aylanismello@gmail.com>
2018-11-04 02:29:00 -0700

    functional component

--- src/BCWeeklyList/BCWeeklyList.js
+++ src/BCWeeklyList/BCWeeklyList.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BCWeeklyItem from '../BCWeeklyItem';
 import './BCWeeklyList.scss';
 
-class BCWeeklyList extends Component {
-  render() {
-    const { playlists } = this.props;
-    return (
-      <div className="BCWeeklyList">
-        {playlists.map((playlist, idx) => (
-          <BCWeeklyItem
-            playlist={playlist}
-            active={this.props.activePlaylistIdx === idx}
-            key={idx}
-            setAsActiveItem={() => {
-              this.props.updateActivePlaylist(idx);
-            }}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const BCWeeklyList = ({ playlists, activePlaylistIdx, updateActivePlaylist }) => {
+  return (
+    <div className="BCWeeklyList">
+      {playlists.map((playlist, idx) => (
+        <BCWeeklyItem
+          playlist={playlist}
+          active={activePlaylistIdx === idx}
+          key={idx}
+          setAsActiveItem={() => {
+            updateActivePlaylist(idx);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default BCWeeklyList;
