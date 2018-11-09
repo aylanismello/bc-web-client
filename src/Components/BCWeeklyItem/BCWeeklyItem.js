77ed343e9b0f01856fcc711adc28a3b7cc9f0735
Aylan Mello <aylanismello@gmail.com>
2018-11-08 18:15:43 -0700

    stuff n things with style. still have a nasty bug tho

--- src/Components/BCWeeklyItem/BCWeeklyItem.js
+++ src/Components/BCWeeklyItem/BCWeeklyItem.js
@@ -1,36 +1,58 @@
 import React from 'react';
+import { Image } from 'cloudinary-react';
 import BCWeeklyTracklist from '../BCWeeklyTracklist';
 import './BCWeeklyItem.scss';
 
 const BCWeeklyItem = ({
- playlist, active, setAsActiveItem, idx, playTrack, activeTrack
+  playlist,
+  active,
+  setAsActiveItem,
+  idx,
+  playTrack,
+  activeTrack
 }) => {
-  // maybe have a choice to listen to mix OR playlist?
-  // maybe like a mix mode?
   const { artwork_url, week_num } = playlist;
   const style = active ? { opacity: 0.1 } : {};
+  // probs set this dynamically, responsively
+  // all stryle here are computed based on React media queries,
+  // so let's reflect that.
+  const width = 400;
+  const top = 150;
+  
   return (
-    <div className="BCWeeklyItem" onClick={() => setAsActiveItem(week_num)} id={idx}>
-      <div className="BCWeeklyItem-cover">
-        <img src={artwork_url} alt="Cover Art" className="BCWeeklyItem-cover-image" style={style} />
+    <div
+      className="BCWeeklyItem"
+      onClick={() => setAsActiveItem(week_num)}
+      id={idx}
+    >
+      <div className="BCWeeklyItem-cover" style={{ width: `${width}px`, height: `${width}px`}}>
+        <Image
+          className="BCWeeklyItem-cover-image"
+          alt="Cover Art"
+          width={width}
+          crop="fit"
+          quality="70"
+          cloudName="burncartel"
+          publicId={artwork_url}
+          style={style}
+        />
         {active && (
-          <div className="BCWeeklyItem-cover-text">
+          <div className="BCWeeklyItem-cover-text" style={{ top: `${top}px` }}>
             <h4> BURN CARTEL WEEKLY </h4>
             <div className="BCWeeklyItem-line" />
             <h4> WEEK {week_num} </h4>
           </div>
         )}
       </div>
-      {active &&
-        playlist.tracks && (
-          <BCWeeklyTracklist
-            idx={idx}
-            tracks={playlist.tracks}
-            activeTrack={activeTrack}
-            playTrack={playTrack}
-            playlist={playlist}
-          />
-        )}
+      {active && playlist.tracks && (
+        <BCWeeklyTracklist
+          idx={idx}
+          tracks={playlist.tracks}
+          activeTrack={activeTrack}
+          playTrack={playTrack}
+          playlist={playlist}
+        />
+      )}
     </div>
   );
 };
