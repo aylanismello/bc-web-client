c51aefaf1c2e7bf8a967101c5f6149c255d162f0
Aylan Mello <aylanismello@gmail.com>
2019-04-17 18:29:51 +0700

    Add player bar to mobile

--- src/Components/BottomNav/BottomNav.js
+++ src/Components/BottomNav/BottomNav.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Responsive from 'react-responsive';
 import { Link } from 'react-router-dom';
 import './BottomNav.scss';
 import PlayerControls from './PlayerControls';
+import SeekBar from '../SeekBar';
 
 const BottomNav = ({
   track,
@@ -28,47 +30,53 @@ const BottomNav = ({
   }
 
   return (
-    <div className="BottomNav">
-      {track && track.id && (
-        <Link
-          onClick={() => {
-            if (window.location.hash.includes(playingCollectionNum)) {
-              forceReopenCollectionDetail();
-            }
-          }}
-          to={collectionLink}
-        >
-          <div className="BottomNav-track-info">
-            <div className="BottomNav-track-info-artwork-container">
-              <img
-                src={track.artwork_url || track.artist_artwork_url}
-                className="Bottom-track-info-artwork"
-                alt="track-artwork"
-                draggable={false}
-              />
+    <div className="BottomNav-Container">
+      <Responsive maxWidth={949}>
+        <SeekBar currentTime={currentTime.raw} showTopSeekBar />
+      </Responsive>
+      <div className="BottomNav">
+      {/* TODO: make the content of this invisible when loading, not the whole div */}
+        {track && track.id && (
+          <Link
+            onClick={() => {
+              if (window.location.hash.includes(playingCollectionNum)) {
+                forceReopenCollectionDetail();
+              }
+            }}
+            to={collectionLink}
+          >
+            <div className="BottomNav-track-info">
+              <div className="BottomNav-track-info-artwork-container">
+                <img
+                  src={track.artwork_url || track.artist_artwork_url}
+                  className="Bottom-track-info-artwork"
+                  alt="track-artwork"
+                  draggable={false}
+                />
+              </div>
+              <div className="BottomNav-track-info-details-container">
+                <span className="BottomNav-track-info-detail BottomNav-track-info-name">
+                  {track.name}
+                </span>
+                <span className="BottomNav-track-info-detail BottomNav-track-info-artist">
+                  {track.artist_name}
+                </span>
+              </div>
             </div>
-            <div className="BottomNav-track-info-details-container">
-              <span className="BottomNav-track-info-detail BottomNav-track-info-name">
-                {track.name}
-              </span>
-              <span className="BottomNav-track-info-detail BottomNav-track-info-artist">
-                {track.artist_name}
-              </span>
-            </div>
-          </div>
-        </Link>
-      )}
-      <PlayerControls
-        playing={playing}
-        trackLoading={trackLoading}
-        goToTrack={goToTrack}
-        togglePlay={togglePlay}
-        repeat={repeat}
-        currentTime={currentTime}
-        visualize={visualize}
-        toggleRepeat={toggleRepeat}
-        toggleVisualize={toggleVisualize}
-      />
+          </Link>
+        )}
+        <PlayerControls
+          playing={playing}
+          trackLoading={trackLoading}
+          goToTrack={goToTrack}
+          togglePlay={togglePlay}
+          repeat={repeat}
+          currentTime={currentTime}
+          visualize={visualize}
+          toggleRepeat={toggleRepeat}
+          toggleVisualize={toggleVisualize}
+        />
+      </div>
     </div>
   );
 };
--- src/Components/SeekBar/SeekBar.js
+++ src/Components/SeekBar/SeekBar.js
@@ -1,7 +1,71 @@
 import React from 'react';
-import './SeekBar.scss';
+import styled from 'styled-components';
 
 // m.dotdev.co/how-to-build-an-audio-player-with-html5-and-the-progress-element-487cbbbaebfc
+
+const SeekBarStyle = styled.div`
+  text-align: center;
+  width: 100%;
+
+  progress,
+  progress[role] {
+    /* Turns off styling - not usually needed, but good to know. */
+    appearance: none;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+
+    /* gets rid of default border in Firefox and Opera. */
+    border: none;
+
+    /* Needs to be in here for Safari polyfill so background images work as expected. */
+    background-size: auto;
+
+    /* Dimensions */
+    width: 100%;
+    border: none;
+    height: 0.5rem;
+    border-radius: ${(props) => props.showTopSeekBar ? '0px' : '300px'};
+
+
+    /* more */
+    
+    position: ${(props) => props.showTopSeekBar ? 'absolute' : ''};
+    left: ${(props) => props.showTopSeekBar ? '0' : ''}; 
+    right: ${(props) => props.showTopSeekBar ? '0' : ''};
+  }
+
+  progress::-webkit-progress-bar {
+    background: #626970;
+    border-radius: ${(props) => props.showTopSeekBar ? '0px' : '300px'};
+    
+  }
+  progress::-webkit-progress-value {
+    background: #e54ea3;
+    border-radius: ${(props) => props.showTopSeekBar ? '0px' : '300px'};
+  }
+
+  progress::-moz-progress-bar {
+    background: #e54ea3;
+    border-radius: ${(props) => props.showTopSeekBar ? '0px' : '300px'};
+  }
+
+  progress::-moz-progress-value {
+    background: #626970;
+    border-radius: ${(props) => props.showTopSeekBar ? '0px' : '300px'};
+    
+  }
+
+  progress:hover {
+    cursor: pointer;
+  }
+
+  @media only screen and (max-width: 949px) {
+    progress[value] {
+      /* height: 1.2rem; */
+      height: 0.5rem;
+    }
+  }
+`;
 
 class SeekBar extends React.Component {
   componentDidMount() {
@@ -14,17 +78,26 @@ class SeekBar extends React.Component {
     });
   }
 
+  getStyle() {
+    return this.props.showTopSeekBar ? { position: 'absolute' } : {};
+  }
+
   render() {
     const { currentTime } = this.props;
     const value = currentTime / window.sc.audio.duration;
     return (
-      <div className="SeekBar">
+      <SeekBarStyle style={this.getStyle()} showTopSeekBar={this.props.showTopSeekBar}>
         <div className="player-controls scrubber">
           <progress id="seek-obj" value={value || 0} max="1" />
         </div>
-      </div>
+
+      </SeekBarStyle>
     );
   }
 }
+
+SeekBar.defaultProps = {
+  showTopSeekBar: false
+};
 
 export default SeekBar;
