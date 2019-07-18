58e7aa21a0b3be0c42bb1ee3ac01ce385e96b633
Aylan Mello <aylanismello@gmail.com>
2019-07-19 02:49:14 +0700

    change copy to Curated from Weekly

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 const getWeeklyItemTexts = ({ collection_type, name, collection_num }) => {
   const isWeekly = collection_type === 0 || collection_type === 'weekly';
 
-  const topText = isWeekly ? `Week ${collection_num}` : name;
+  const topText = isWeekly ? `Curated ${collection_num}` : name;
   const bottomText = isWeekly ? 'Burn Cartel Curated' : 'Burn Cartel Rising';
 
   const collectionDescription = isWeekly
