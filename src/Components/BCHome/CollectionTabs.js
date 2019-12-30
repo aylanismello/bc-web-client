39d998098e580484a7b2586182d37bba23ed5d30
Aylan Mello <aylanismello@gmail.com>
2019-12-29 17:00:55 -0800

    curated phrasing changed to radio

--- src/Components/BCHome/CollectionTabs.js
+++ src/Components/BCHome/CollectionTabs.js
@@ -90,7 +90,7 @@ export default ({
           selected={collectionTypeSelected === 0}
         >
           {' '}
-          Curated{' '}
+          Radio{' '}
         </CollectionTab>
         <CollectionTab
           onClick={() => {
--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,8 @@
 const getWeeklyItemTexts = ({ collection_type, name, collection_num }) => {
   const isWeekly = collection_type === 0 || collection_type === 'weekly';
 
-  const topText = isWeekly ? `Curated ${collection_num}` : name;
-  const bottomText = isWeekly ? 'Burn Cartel Curated' : 'Burn Cartel Rising';
+  const topText = isWeekly ? `Episode ${collection_num}` : name;
+  const bottomText = isWeekly ? 'Burn Cartel Radio' : 'Burn Cartel Rising';
 
   const collectionDescription = isWeekly
     ? `
